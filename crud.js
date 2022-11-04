1eb27a66864ff662b9c244b98e31b51bdca4b9c6
Nibardo Vera Diaz <Lic_utel@hotmail.com>
2022-11-03 23:27:54 -0600

    ultimos arreglos CRUD

--- crud.js
+++ crud.js
@@ -84,6 +84,76 @@ function editar(titulo){
     }
 }
 
+//crear la funcion actualizar
 
+function actualizar(i){
+    let libros = JSON.parse(localStorage.getItem("libros"));
+    libros[i].titulo=document.getElementById("newtitulo").value;
+    libros[i].descripcion=document.getElementById("newdescripcion").value;
+    
+
+        localStorage.setItem("libros", JSON.stringify(libros));
+        vistaPrincipal();
+
+}
+
+//crear la funcion eliminar
+function eliminar(titulo){
+    let libros =JSON.parse(localStorage.getItem("libros"));
+    for(let i=0; i < libros.length; i ++){
+        if (libros[i].titulo === titulo){
+            libros.splice(i,1);
+        }
+    
+    }
+
+localStorage.setItem("libros", JSON.stringify(libros));
+leer();
+}
+
+//funcion para mostrar la interfaz principal
+function vistaPrincipal(){ 
+    document.getElementById("body").innerHTML = `<div class="row">
+    <div class="col-md-5">
+        <div class="card">
+            <div class="card-header">
+                <h2>Agregar nuevo libro</h2>
+            </div>
+            <div class="card-body">
+                <form id="formulario">
+                    <div class="form-group">
+                        <input type="text" id="titulo" class="form-control my-3" placeholder="Ingresar título">
+                    </div>
+                    <div class="form-group">
+                        <textarea id="descripcion" class="form-control my-3" placeholder="Ingresar descripción"></textarea>
+                    </div>
+                    
+                    <button type="submit" class="btn btn-primary">Agregar</button>
+                </form>
+
+            </div>
+        </div>
+    </div>
+    <div class="col-md-6">
+        <table class="table caption-top bg-light">
+            <thead>
+              <tr>
+                <th scope="col">Tarea</th>
+                <th scope="col">Descripción</th>
+                
+              </tr>
+            </thead>
+            <tbody id="tbody">
+              <tr>
+                <td>Elaborar proyecto 2 CRUD</td>
+                <td>Revisar contenido en Teams</td>
+                
+              </tr>
+            </tbody>
+          </table>
+    </div>
+</div>`
+leer();
+}
 
 leer();
